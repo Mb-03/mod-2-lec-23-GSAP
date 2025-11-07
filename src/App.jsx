b55f679c4dd978e46785b2f0948e207280350306
MB <baratashvilimisho@gmail.com>
2025-11-07 23:07:47 +0400

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Cocktails from "./components/Cocktails";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
     <div className="w-full overflow-x-hidden">
       <Navbar />
       <Hero />
+      <Cocktails />
     </div>
   );
 };
